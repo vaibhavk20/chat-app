bd11af105f0b9ce150b2dc073acc872babe83dfb
Vaibhav <vaibhav.2071998@gmail.com>
2023-06-14 00:56:39 +0530

    added production code

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
@@ -21,6 +22,24 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------Deployment----------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------Deployment----------------------------
 
 // Error Handling middlewares
 app.use(notFound);
