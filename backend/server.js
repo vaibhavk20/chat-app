03b560287b2b9d6e9e3d4f4405f0b1f968587bc3
Vaibhav <vaibhav.2071998@gmail.com>
2023-06-14 23:45:53 +0530

    added socket.io disconnection

--- backend/server.js
+++ backend/server.js
@@ -90,4 +90,9 @@ io.on("connection", (socket) => {
       socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
